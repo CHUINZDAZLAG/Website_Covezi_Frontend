1e314fba5047f19a75250a8d85a2de37cb5a90b4
trander <thevinh15925@gmail.com>
2025-07-12 11:48:07 +0700

    l-09

--- src/main.jsx
+++ src/main.jsx
@@ -17,6 +17,9 @@ import { BrowserRouter } from 'react-router-dom'
 import { PersistGate } from 'redux-persist/integration/react'
 import { persistStore } from 'redux-persist'
 const persistor = persistStore(store)
+// Inject store technique: là kỹ thuật khi cần sử dụng biến redux store ở các file ngoài phạm vi component
+import { injectStore } from './utils/authorizeAxios'
+injectStore(store)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter basename='/'>
--- src/redux/user/userSlice.js
+++ src/redux/user/userSlice.js
@@ -1,6 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import authorizedAxiosInstance from '~/utils/authorizeAxios'
 import { API_ROOT } from '~/utils/constants'
+import { toast } from 'react-toastify'
 
 // Initialize value of a Slice in Redux
 const initialState = {
@@ -9,10 +10,21 @@ const initialState = {
 
 // Actions to call api (asynchronous) and update data to Redux, use middleware createAsyncThunk go with extraReducers
 export const loginUserAPI = createAsyncThunk(
-  'activeBoard/loginUserAPI',
+  'user/loginUserAPI',
   async (data) => {
     const response = await authorizedAxiosInstance.post(`${API_ROOT}/v1/users/login`, data)
     // Axios return json data in response.data
+    return response.data
+  }
+)
+
+export const logoutUserAPI = createAsyncThunk(
+  'user/logoutUserAPI',
+  async (showSuccessMessage = true) => {
+    const response = await authorizedAxiosInstance.delete(`${API_ROOT}/v1/users/logout`)
+    if (showSuccessMessage) {
+      toast.success('Logged out successfully!')
+    }
     return response.data
   }
 )
@@ -28,6 +40,13 @@ export const userSlice = createSlice({
     builder.addCase(loginUserAPI.fulfilled, (state, action) => {
       const user = action.payload
       state.currentUser = user
+    })
+    builder.addCase(logoutUserAPI.fulfilled, (state) => {
+    /**
+     * API logout sau khi gọi thành công thì sẽ clear thông tin currentUser về null ở đây
+     * Kết hợp ProtectedRoute đã làm ở App.js => code sẽ điều hướng chuẩn về trang Login
+     */
+      state.currentUser = null
     })
   }
 })
